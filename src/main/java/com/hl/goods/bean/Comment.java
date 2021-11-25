9239187cbadb924688bc8f0edd7c1aeb8d345efa
elias <elias_git@qq.com>
2021-11-25 15:22:09 +0800

    管理员功能大致完成

---
+++ src/main/java/com/hl/goods/bean/Comment.java
@@ -0,0 +1,29 @@
+package com.hl.goods.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @author hl2333
+ */
+@Entity
+@Table(name = "comment")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "[needId]")
+    private int needId;
+
+    @Column(name = "[content]")
+    private String content;
+}
